8e97952302b9d6b10d9350c9e0c0b67203d43639
Abdella <abdella.shukur2@gmail.com>
2022-07-25 14:02:51 -0400

    each thread is running at appropriate time

--- src/main/java/Pizza.java
+++ src/main/java/Pizza.java
@@ -1,5 +1,27 @@
 public class Pizza {
   //Fix this method
+
+  
+// You’re given a `Pizza` class that has methods for each step of cooking the
+// pizza. Each of the methods except for the `cookPizza` method extends the
+// `Thread` class and overrides the `run` method.
+
+// The `cookPizza` method is not giving the expected output. Your task is to fix it
+// so that the output looks like the following:
+
+// ```plaintext
+// cook base
+// cover base with sauce
+// slice tomatoes 3
+// slice tomatoes 2
+// slice tomatoes 1
+// put tomatoes on pizza
+// grate cheese on pizza
+// to bake...4 min
+// to bake...3 min
+// to bake...2 min
+// to bake...1 min
+
   public static void cookPizza() throws InterruptedException {
 
       Base base = new Base();
@@ -17,7 +39,9 @@ public class Pizza {
 
       for (Thread step : cookingSteps) {
           step.start();
+          step.join();
       }
+
   }
 
   // DO NOT change any of the methods below
